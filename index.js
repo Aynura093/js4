94ea6d23be5e550d486c0503186d6d1d64d676c6
Aynura093 <aynurazemu@gmail.com>
2024-08-23 10:36:53 -0700

    js4homework

---
+++ index.js
@@ -0,0 +1,29 @@
+let fullName = "Ibrahimova Aynure"
+ console.log ( fullName)
+ let name = fullName.split(" ")
+ console.log (name)
+ console.log ( name [0][0])
+ console.log ( name [1][0])
+ 
+ let name = prompt ("AD SOYAD")
+ console.log(name)
+ fullName = name.split(" ")
+ console.log (fullName)
+ console.log ( fullName[1] + " " + fullName[0])
+ 
+let a = prompt("Soyadınızı yazın")
+console.log(a)
+console.log( a.length)
+console.log(a.includes("e"))
+console.log(a.startsWith("code"))
+console.log(a.endsWith("code"))
+console.log(a.replaceAll("t","####"))
+
+
+ 
+
+
+
+
+
+
